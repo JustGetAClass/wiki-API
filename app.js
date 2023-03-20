24632561e7814647350a163e6dda05afc2f498d3
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-20 12:36:00 +0300

    database setup

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+main().catch((err) => console.log(err));
+
+async function main() {
+	const url = "mongodb://127.0.0.1:27017/wikiDB";
+	await mongoose.connect(url, { useNewUrlParser: true });
+}
+
+const articleSchema = new mongoose.Schema({
+	title: String,
+	content: String,
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}`);
 });
